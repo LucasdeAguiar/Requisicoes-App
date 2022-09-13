9df44aa669d44f607e15386bc1ce3e5240515fef
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2022-09-13 16:42:37 -0300

    adicionado envioroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCJdT9eTPqIcEZaS5rGnaAXDyn0TN2NCns",
+    authDomain: "requisicoes-app-435f4.firebaseapp.com",
+    projectId: "requisicoes-app-435f4",
+    storageBucket: "requisicoes-app-435f4.appspot.com",
+    messagingSenderId: "664306693461",
+    appId: "1:664306693461:web:ea3347be03e93afc9debdc"
+  }
 };
